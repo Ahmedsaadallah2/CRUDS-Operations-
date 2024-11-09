dbb7bdbbe70e30db99c89dc61373a2f7f9f8171f
Ahmedsaadallah2 <atoshatosh55@gmail.com>
2024-11-09 17:50:05 +0200

    Custom Model Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,42 @@
+import { useState } from "react";
 import Card from "./components/ProductCard/Card";
+import Modal from "./components/UI/Model";
 import { productList } from "./data";
 
 function App() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  function closeModal() {
+    setIsOpen(false);
+  }
+
+  function openModal() {
+    setIsOpen(true);
+  }
   const product = productList.map((pro) => <Card key={pro.id} product={pro} />);
   return (
     <>
       <main className="container">
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-3 m-3">
+        <button
+          onClick={openModal}
+          className="bg-indigo-500 p-2 w-full rounded-md text-white"
+        >
+          Add Product
+        </button>
+        <Modal isOpen={isOpen} closeModal={closeModal} title="Add Product">
+          <div className="flex space-x-2">
+            <button
+              onClick={closeModal}
+              className="bg-indigo-500 p-2 w-full rounded-md text-white"
+            >
+              Cancel
+            </button>
+            <button className="bg-gray-300 p-2 w-full rounded-md text-white hover:bg-gray-600 transition">
+              Submit
+            </button>
+          </div>
+        </Modal>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-3 m-3">
           {product}
         </div>
       </main>
